f8d46f299db3bedce4319ef096ad3b5b02d5ebf5
jaq-cat <jaq.cat@protonmail.ch>
2020-12-05 10:46:24 -0300

    e

--- src/snake.c
+++ src/snake.c
@@ -36,7 +36,7 @@ void add_node(Snake *snek, Point p) {
     snek->len += 1;
 }
 
-int move(Snake *snek, int x, int y) {
+int move(Snake *snek, int y, int x) {
     SnakeNode *head = snek->tail->next;
     head->data = (Point) { head->data. y+ y, head->data.x + x };
     SnakeNode *node = snek->tail;
